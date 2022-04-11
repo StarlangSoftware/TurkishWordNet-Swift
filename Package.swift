e23374c7599488ca67f1516239822d5d3c01919e
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-11 11:35:28 +0300

    Updated previous versions of WordNets.

--- Package.swift
+++ Package.swift
@@ -19,6 +19,14 @@ let package = Package(
         .target(
             name: "WordNet",
             dependencies: ["MorphologicalAnalysis"],
+            exclude: ["turkish1944_dictionary.txt", "turkish1944_wordnet.xml",
+                      "turkish1955_dictionary.txt", "turkish1955_wordnet.xml",
+                      "turkish1959_dictionary.txt", "turkish1959_wordnet.xml",
+                      "turkish1966_dictionary.txt", "turkish1966_wordnet.xml",
+                      "turkish1969_dictionary.txt", "turkish1969_wordnet.xml",
+                      "turkish1974_dictionary.txt", "turkish1974_wordnet.xml",
+                      "turkish1983_dictionary.txt", "turkish1983_wordnet.xml",
+                      "turkish1988_dictionary.txt", "turkish1988_wordnet.xml"],
             resources: [.process("turkish_wordnet.xml"),.process("english_wordnet_version_31.xml"),.process("english_exception.xml")]),
         .testTarget(
             name: "WordNetTests",
